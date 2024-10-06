dc7856ed399a657c4e1e4e9ffe94b0af81d6ce7d
nk0-0 <kozak.na@phystech.edu>
2024-10-06 18:44:09 +0400

    add cpp project vec2d

---
+++ MyCpp/build/08_project/vec/CMakeFiles/vec.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/user/Desktop/My/MyCpp/lib/vec/vec2d.cpp" "08_project/vec/CMakeFiles/vec.dir/__/__/lib/vec/vec2d.cpp.obj" "gcc" "08_project/vec/CMakeFiles/vec.dir/__/__/lib/vec/vec2d.cpp.obj.d"
+  "C:/Users/user/Desktop/My/MyCpp/08_project/vec/vec.cpp" "08_project/vec/CMakeFiles/vec.dir/vec.cpp.obj" "gcc" "08_project/vec/CMakeFiles/vec.dir/vec.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MyCpp/build/08_project/vec/CMakeFiles/vec.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vec.dir/__/__/lib/vec/vec2d.cpp.obj"
+  "CMakeFiles/vec.dir/__/__/lib/vec/vec2d.cpp.obj.d"
+  "CMakeFiles/vec.dir/vec.cpp.obj"
+  "CMakeFiles/vec.dir/vec.cpp.obj.d"
+  "libvec.dll.a"
+  "vec.exe"
+  "vec.exe.manifest"
+  "vec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
